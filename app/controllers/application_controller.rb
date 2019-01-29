4a82ba6ab65d2cf466292588addb262158865396
rschmelter <rschmelter33@gmail.com>
2019-01-28 17:41:24 -0800

    add login, signup, show to users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::API
 
+
+private
     def current_user
        user = User.find(params[:id])
         user
     end
+
+helper_method :current_user
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,33 @@ class UsersController < ApplicationController
 
     def create
         user = User.new(user_params)
+        if user.save
+            render json: current_user
+        else
+            render json: {error: 'Failed to Sign Up'}, status: 400
+        else
+            
+        end
+    end
+
+    def login 
+        user = User.find_by(username: params[:user][:username])
+        if user.password = params[:user][:password]
+            render json: current_user
+        else 
+            render json: {error: 'Failed to Log In'}, status: 400
+        end
+
+    end
+
+    def show
+        render json: current_user
+    end 
+
+private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
     end
 
 end 

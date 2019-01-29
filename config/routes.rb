3bfc67ee7f4fd585a566baeb103ab07ae975bfda
rschmelter <rschmelter33@gmail.com>
2019-01-28 16:26:52 -0800

    draw routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+  resources :users
+  post '/signup', to: 'users#signup'
+  post '/login', to: 'users#login'
+  get '/logout', to: 'users#logout'
+
+  resources :decks do
+    resources :cards
+  end
+
+
+
+
 end

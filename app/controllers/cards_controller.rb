65a805cd7cd4d42876431ac1501513f9a78c6a1d
rschmelter <rschmelter33@gmail.com>
2019-01-29 16:25:42 -0800

    fill out cards controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,3 +1,38 @@
 class CardsController < ApplicationController
 
+    def index 
+        deck = current_user.decks.find(params[:deck_id].to_i)
+        cards = deck.cards.all 
+
+        render json: cards
+    end
+
+    def show
+        card = Card.find(params[:id])
+        render json: card
+    end 
+
+    def create
+        deck = current_user.decks.find(params[:deck_id].to_i)
+        card = deck.cards.build(card_params)
+        card.save
+
+        render json: card 
+    end
+
+    def destroy 
+        card = Card.find(params[:id])
+        card.destroy
+
+        render json: card 
+
+    end
+
+    private
+
+    def card_params
+        params.require(:card).permit(:front, :back, :difficulty, :dek_id)
+
+    end 
+
 end 

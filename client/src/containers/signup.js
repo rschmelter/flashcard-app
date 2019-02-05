4388077b90dcbe64ffa11e1c695c4bc2de4afb54
rschmelter <rschmelter33@gmail.com>
2019-02-04 16:15:59 -0800

    add form field to signup

--- client/src/containers/signup.js
+++ client/src/containers/signup.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+
 
 class SignUp extends Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            username: '',
+            password: ''
+        };
+    }
+
+
+
+
     render() {
       return (
 
-          <div className="Start">
-            <h1>Goodbye</h1>
+          <div className="signup">
+            <form onSubmit={this.handleSubmit}>
+                    <label>signup</label>
+                <input type="text" onChange={(event) => this.handleChange(event)} value={this.state.text}/>
+                <input type="submit"/>
+            </form>
           </div>
 
       );

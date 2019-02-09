93dd824e7895e507983949f2a904f5082d236dd1
rschmelter <rschmelter33@gmail.com>
2019-02-09 08:02:44 -0800

    add DeckForm to containers

---
+++ client/src/containers/DeckForm.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { createDeck } from '../actions/deckActions'
+import { bindActionCreators } from 'redux'
+
+
+
+class DeckForm extends Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            name: ""
+        };
+    }
+
+
+    handleChange = event => {
+        const field = event.target.name
+        let state = this.state
+
+        state[field] = event.target.value
+        this.setState(state)
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        this.props.login(this.state)
+    }
+
+
+
+    render() {
+      return (
+
+          <div className="DeckForm">
+            <form onSubmit={this.handleSubmit}>
+                    <label>Login</label>
+                <input name="name" placeholder="Name" type="text" onChange={(event) => this.handleChange(event)} value={this.state.username}/>
+                <input type="submit"/>
+            </form>
+          </div>
+
+      );
+    }
+  }
+
+  const mapDispatchToProps = dispatch => bindActionCreators({
+    createDeck }, dispatch)
+
+  
+  export default  connect(null, mapDispatchToProps)(DeckForm);

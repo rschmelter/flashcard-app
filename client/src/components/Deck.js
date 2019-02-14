ed1ccbd1a09e93bb71a9c16e0779aa1204185714
rschmelter <rschmelter33@gmail.com>
2019-02-14 12:28:40 -0800

    Deck components receives props

--- client/src/components/Deck.js
+++ client/src/components/Deck.js
@@ -3,16 +3,16 @@ import React, { Component } from 'react';
 
 class Deck extends Component {
   render() {
-   
+
     return (
     
       
         <div className="Deck">
-            
+            {this.props.deck}
         </div>
 
     );
   }
 }
 
-export default Home;
+export default Deck;

e2e657abe7a41a319269ea4f891b7d5cd86c59f2
rschmelter <rschmelter33@gmail.com>
2019-02-16 07:34:27 -0800

    add card reducer to combine reducers

---
+++ client/src/reducers/cardReducer.js
@@ -0,0 +1,19 @@
+const initialState = {
+ 
+    all: []
+}
+
+export default function cardReducer(state = initialState, action) {
+  
+    switch(action.type) {
+        case 'CREATE_CARD':
+            return {...state, all: [...state.all, action.payload]}
+
+        case 'FETCH_CARDS':
+
+        return {...state, all: action.payload}
+            
+     
+        default: return state
+    }
+}
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 
 import userReducer from './userReducer'
 import deckReducer from './deckReducer'
+import cardReducer from './cardReducer'
 
 
 const rootReducer = combineReducers({
   user: userReducer,
-  deck: deckReducer
+  deck: deckReducer,
+  card: cardReducer
 
 })
 

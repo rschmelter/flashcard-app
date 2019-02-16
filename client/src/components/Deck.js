ffe91ab4594218a799642f519be883ffb6abf80d
rschmelter <rschmelter33@gmail.com>
2019-02-16 06:37:45 -0800

    add link to in Deck component

--- client/src/components/Deck.js
+++ client/src/components/Deck.js
@@ -8,8 +8,7 @@ class Deck extends Component {
     
       
         <div className="Deck">
-            <h2>{this.props.deckId}</h2>
-            <h2>{this.props.deck}</h2>
+
             <Link to={`/decks/${this.props.deckId}`}><h2>{this.props.deck}</h2></Link>
         </div>
 

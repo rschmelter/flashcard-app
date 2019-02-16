ff4fb276e863e4f05f5ed35dd5233386ed74874e
rschmelter <rschmelter33@gmail.com>
2019-02-16 06:59:32 -0800

    add CarList component

---
+++ client/src/containers/CardList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class CardList extends Component {
+  render() {
+
+    return (
+    
+      
+        <div className="CardList">
+
+            
+        </div>
+
+    );
+  }
+}
+
+export default CardList;

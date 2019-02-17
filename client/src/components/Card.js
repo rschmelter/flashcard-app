f8c3b705355482e0c982af2ed466f02874bbd22c
rschmelter <rschmelter33@gmail.com>
2019-02-17 09:50:08 -0800

    add handleClick to toggle between front and back on Card

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
 
 
-class Deck extends Component {
+class Card extends Component {
+
+  state = {
+    front: true
+  }
+
+  handleClick = () => {
+    this.setState({front: !this.state.front})
+  }
+
   render() {
+
+
 
     return (
     
       
-        <div className="Deck">
-
-            <h1>{this.props.front}</h1>
-            <h1>{this.props.back}</h1>
+        <div className="Card" onClick={this.handleClick}>
+          {
+            this.state.front
+            ? <h1>{this.props.front}</h1>
+            : <h1>{this.props.back}</h1>
+          }
         </div>
 
     );
   }
 }
 
-export default Deck;
+export default Card;
